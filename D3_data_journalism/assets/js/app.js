fd42a0a1f3fa3c796d8bed51bda264d1b235c709
ashparra97 <ashparra97@arizona.edu>
2021-07-08 18:19:30 -0700

    Update app.js

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -1,1 +1,21 @@
-// @TODO: YOUR CODE HERE!
+// Generate SVG 
+
+var svg = d3.select("body")
+    .append("svg");
+
+// Update attributes in svg 
+svg.attr("width", "100px")
+    .attr("height", "100px");
+
+
+
+
+
+// Import Data 
+
+//d3.csv("").then(function(NAMEOFFUNCTION) {
+
+
+
+
+//});
